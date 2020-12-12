049d742da1f824157a8ed5ae61f97e2468e40f72
Rafael Reyes <rafaelalexanderr5@gmail.com>
2020-12-12 01:44:55 -0400

    Created - all the components needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProfileComponent } from './components/details/profile/profile.component';
+import { CommentsComponent } from './components/details/comments/comments.component';
+import { ResultsComponent } from './components/search/results/results.component';
+import { EngineComponent } from './components/search/engine/engine.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ProfileComponent,
+    CommentsComponent,
+    ResultsComponent,
+    EngineComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
