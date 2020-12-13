2e03920feede6355870b917b82eda8033f200d64
Rafael Alexander Reyes Perez <rafaelalexanderr5@gmail.com>
2020-12-13 17:44:42 -0400

    COMPLETED - Search engine filter

--- src/app/components/search/engine/engine.component.ts
+++ src/app/components/search/engine/engine.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { Observable, Subject, merge } from 'rxjs';
-import { NgbTypeaheadConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTypeaheadConfig,  NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
 import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
 import { BusinessService } from '../../../services/business.service';
 
@@ -13,20 +13,12 @@ import { BusinessService } from '../../../services/business.service';
 export class EngineComponent {
   title = "Find your business with us";
   slogan = "We provide the information that you are looking for";
-  names = [];
+  names = <any>[];
   id = <any>[];
-  country = "";
+  country =  <any>[];
   categories = <any>[];
   data = <any>[];
 
-  getCountry(item) {
-    const name = item.value;
-    for(let i = 0; i < this.data.length; i++){
-      if(this.data[i].name === name){
-        this.country = this.data[i].country;
-      }
-    }
-  }
   constructor(public bussiness: BusinessService, config: NgbTypeaheadConfig) {
     // customize default values of typeaheads used by this component tree
     config.showHint = true;
@@ -35,18 +27,22 @@ export class EngineComponent {
       this.data = data;
       // console.log(this.data)
       // Getting the business id
-      this.data.forEach(el => {
+      this.data.forEach((el: { name: any; country: any; categories: any[]; }) => {
         this.names.push(el.name);
+        this.country.push(el.country);
         el.categories.forEach(el => {
-          this.categories.push(el.name);
+          this.categories.push({name: el.name});
         });
       });
+      console.log(this.categories)
     });
   }
-
+  businessSelected: any;
+  businessCategory: any;
   model: any;
 
-  @ViewChild('instance', { static: true }) instance: NgbTypeahead;
+  @ViewChild('instance', { static: true })
+  instance!: NgbTypeahead;
   focus$ = new Subject<string>();
   click$ = new Subject<string>();
 
@@ -57,7 +53,7 @@ export class EngineComponent {
 
     return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
       map((term, index) => (term === '' ? this.names
-        : this.names.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10))
+        : this.names.filter((v: string) => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10))
     );
   }
 }
